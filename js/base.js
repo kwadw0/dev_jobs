15b04729b61f8a9477118f835937978180a1e642
kwadw0 <kwadwoasani@gmail.com>
2023-06-22 16:15:59 +0000

    added a gitignore file.

--- js/base.js
+++ js/base.js
@@ -1,148 +1,124 @@
-// let http = new XMLHttpRequest();
 
-// http.open("get", "../json_data/data.json", true);
+const url = "../json_data/data.json";
+const desiredId = 1;
+const detailedUrl = `../json_data/data.json`;
 
-// http.send();
+// Assuming jsonData is the parsed array of objects from your JSON file
 
-// http.onload = function(){
-//     if(this.readyState == 4 && this.staus == 200){
-//         let job_card = JSON.parse(this.responseText);
 
-//         output = "";
+fetch(`${detailedUrl}`)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
 
-//         for(let jobs in job_card){
-//             output += `
-//                 <div class="job_card">
-//                 <div class="job-logo">
-//                 <img
-//                     class="logo"
-//                     style=".${jobs.logoBackground}"
-//                     src="${jobs.logo}"
-//                     alt=""
-//                 />
-//                 </div>
-//                 <div class="job-info">
-//                 <div class="job-time">
-//                     <p>${jobs.postedAt} . ${jobs.contract}</p>
-//                 </div>
-//                 <div class="job-title">
-//                     <h2>${jobs.position}</h2>
-//                 </div>
-//                 <div class="job-company">
-//                     <p>${jobs.company}</p>
-//                 </div>
-//                 <div class="job-location">
-//                     <h3>${jobs.location}</h3>
-//                 </div>
-//                 </div>
-//                 </div>
-// //             `
-//         }
-//         console.log(output);
-//         document.querySelector(".job_card").innerHTML = output;
-//     }
+    const detail_placeholder = document.querySelector(".company-info-container");
 
-// }
+    let detail_output = "";
 
-fetch("../json_data/data.json")
+    data.forEach(function(job) {
+      detail_output += `
+        <div class="company-info">
+          <p id="job-detail-type">${job.postedAt} . ${job.contract}</p>
+          <h2 class="job-detail-title">${job.position}</h2>
+          <p id="job-detail-location">${job.location}</p>
+          <div class="company-site2-button">
+            <button type="submit" id="company-site2">Apply Now</button>
+          </div>
+          <p id="about-company">
+          </p>
+          <h2 id="company-requirements">Requirements</h2>
+          <p id="about-company">
+          ${job.requirements.content}
+          </p>
+          <ul id="requirement-list">`;
+
+      job.requirements.items.forEach(function(item) {
+        detail_output += `<li>${item}</li>`;
+      });
+
+      detail_output += `
+          </ul>
+          <h2 id="company-requirements">What you will do</h2>
+          <p id="about-company">${job.role.content}
+          </p>
+          <ol id="requirement-list">`;
+
+      job.role.items.forEach(function(item) {
+        detail_output += `<li>${item}</li>`;
+      });
+
+      detail_output += `
+          </ol>
+        </div>`;
+    });
+    detail_placeholder.innerHTML = detail_output;
+  });
+
+
+
+fetch(url)
   .then(function (response) {
     return response.json();
   })
-
   .then(function (data) {
-    let placeholder = document.querySelector(".job_container");
-    let detail_placeholder  = document.querySelector("company-info-container");
+    const placeholder = document.querySelector(".job_container");
 
-    output = "";
-    detail_output = "";
+
+    let output = "";
+
 
     for (let jobs of data) {
       output += `
-                <div class="job_card">
-                <div class="job-logo">
-                <img
-                    class="logo"
-                    style="background-color:${jobs.logoBackground}"
-                    src=".${jobs.logo}"
-                    alt=""
-                />
-                </div>
-                <div class="job-info">
-                <div class="job-time">
-                    <p>${jobs.postedAt} . ${jobs.contract}</p>
-                </div>
-                <div class="job-title">
-                    <h2>${jobs.position}</h2>
-                </div>
-                <div class="job-company">
-                    <p>${jobs.company}</p>
-                </div>
-                <div class="job-location">
-                    <h3>${jobs.location}</h3>
-                </div>
-                </div>
-                </div>`;
-    }
-    for(let detail_job of data){
-        detail_output += `
-        <div class="company-info">
-        <p id="job-detail-type">${detail_job.postedAt} . ${detail_job.contract}</p>
-        <h2 class="job-detail-title">${detail_job.position}</h2>
-        <p id="job-detail-location">United Kingdom</p>
-        <div class="company-site2-button">
-          <button type="submit" id="company-site2">Apply Now</button>
+      <a class="detail-url" href="../Template/detail.html?id=${jobs.id}">
+        <div class="job_card">
+          <div class="job-logo">
+            <img
+              class="logo"
+              style="background-color:${jobs.logoBackground}"
+              src=".${jobs.logo}"
+              alt=""
+            />
+          </div>
+          <div class="job-info">
+            <div class="job-time">
+              <p>${jobs.postedAt} . ${jobs.contract}</p>
+            </div>
+            <div class="job-title">
+              <h2>${jobs.position}</h2>
+            </div>
+            <div class="job-company">
+              <p>${jobs.company}</p>
+            </div>
+            <div class="job-location">
+              <h3>${jobs.location}</h3>
+            </div>
+          </div>
         </div>
-      <p id="about-company">
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus
-        hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet
-        vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin
-        laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor
-        eu nibh. Nullam mollis. Ut justo. Suspendisse potenti. Sed egestas,
-        ante et vulputate volutpat, eros pede semper est, vitae luctus metus
-        libero eu augue. Morbi purus libero, faucibus adipiscing, commodo
-        quis, gravida id, est. Sed lectus. Praesent elementum hendrerit
-        tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a
-        ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien
-        ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus
-        sed, urna.
-      </p>
-      <h2 id="company-requirements">Requirements</h2>
-      <p id="about-company">
-        Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a
-        ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis
-        libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing
-        varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam
-        pellentesque mauris ut lectus. Nunc tellus ante, mattis eget, gravida
-        vitae, ultricies ac, leo. Integer leo pede, ornare a, lacinia eu,
-        vulputate vel, nisl.
-      <ul id="requirement-list">
-        <li>Morbi interdum mollis sapien. Sed</li>
-        <li>Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam
-          pellentesque mauris ut lectus.</li>
-        <li>Morbi interdum mollis sapien. Sed</li>
-        <li>Morbi interdum mollis sapien. Sed</li>
-      </ul>
-      </p>
-      <h2 id="company-requirements">What you will do</h2>
-      <p id="about-company">Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus
-        libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed
-        semper lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc
-        sapien ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna.
-      </p>
-      <ol id="requirement-list">
-        <li>Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam
-          pellentesque mauris ut lectus.</li>
-        <li>Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam
-          pellentesque mauris ut lectus.</li>
-        <li>Morbi interdum mollis sapien. Sed</li>
-        <li>Morbi interdum mollis sapien. Sed</li>
-      </ol>
-    </div>
-        
-        `
+      </a>`;
+      
+
     }
-    placeholder.innerHTML = output;
-    console.log(output);
     
+    placeholder.innerHTML = output;
+
   })
-  ;
+
+
+
+// Extract the ID from the URL
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+
+// Search for the object with the matching ID
+const matchingObject = url.find(obj => obj.id === id);
+
+// Display the object
+if (matchingObject) {
+  console.log(matchingObject); // Display the object in the console
+  // Display the object properties on a webpage or perform other actions
+} else {
+  console.log('Object not found');
+  // Handle the case when the object with the provided ID is not found
+}
