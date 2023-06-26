fea079e0ae91d20188ad91cf27ca6d0f1d59a053
kwadw0 <kwadwoasani@gmail.com>
2023-06-26 15:19:17 +0000

    Detail view page now displays the detailed data of the dev jobs.

--- js/base.js
+++ js/base.js
@@ -1,61 +1,4 @@
-
 const url = "../json_data/data.json";
-const desiredId = 1;
-const detailedUrl = `../json_data/data.json`;
-
-// Assuming jsonData is the parsed array of objects from your JSON file
-
-
-fetch(`${detailedUrl}`)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-
-    const detail_placeholder = document.querySelector(".company-info-container");
-
-    let detail_output = "";
-
-    data.forEach(function(job) {
-      detail_output += `
-        <div class="company-info">
-          <p id="job-detail-type">${job.postedAt} . ${job.contract}</p>
-          <h2 class="job-detail-title">${job.position}</h2>
-          <p id="job-detail-location">${job.location}</p>
-          <div class="company-site2-button">
-            <button type="submit" id="company-site2">Apply Now</button>
-          </div>
-          <p id="about-company">
-          </p>
-          <h2 id="company-requirements">Requirements</h2>
-          <p id="about-company">
-          ${job.requirements.content}
-          </p>
-          <ul id="requirement-list">`;
-
-      job.requirements.items.forEach(function(item) {
-        detail_output += `<li>${item}</li>`;
-      });
-
-      detail_output += `
-          </ul>
-          <h2 id="company-requirements">What you will do</h2>
-          <p id="about-company">${job.role.content}
-          </p>
-          <ol id="requirement-list">`;
-
-      job.role.items.forEach(function(item) {
-        detail_output += `<li>${item}</li>`;
-      });
-
-      detail_output += `
-          </ol>
-        </div>`;
-    });
-    detail_placeholder.innerHTML = detail_output;
-  });
-
 
 
 fetch(url)
@@ -71,7 +14,7 @@ fetch(url)
 
     for (let jobs of data) {
       output += `
-      <a class="detail-url" href="../Template/detail.html?id=${jobs.id}">
+      <a class="detail-url"  href="../Template/detail.html?id=${jobs.id}" >
         <div class="job_card">
           <div class="job-logo">
             <img
@@ -98,27 +41,12 @@ fetch(url)
         </div>
       </a>`;
       
-
+      function detailViewUrl(){
+        window.location.href = `../Template/detail.html?id=${jobs.id}`;
+      }
     }
+    
     
     placeholder.innerHTML = output;
 
   })
-
-
-
-// Extract the ID from the URL
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get('id');
-
-// Search for the object with the matching ID
-const matchingObject = url.find(obj => obj.id === id);
-
-// Display the object
-if (matchingObject) {
-  console.log(matchingObject); // Display the object in the console
-  // Display the object properties on a webpage or perform other actions
-} else {
-  console.log('Object not found');
-  // Handle the case when the object with the provided ID is not found
-}
---
+++ js/detailView.js
@@ -0,0 +1,79 @@
+const detailedUrl = "../json_data/data.json";
+
+// Get the job ID from the URL parameter
+const urlParams = new URLSearchParams(window.location.search);
+const jobId = parseInt(urlParams.get("id")); // Parse the ID as an integer
+
+fetch(detailedUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+
+    // Find the job with the matching ID
+    const job = data.find(function (job) {
+      return job.id === jobId;
+    });
+
+    if (job) {
+      const detail_placeholder = document.querySelector(
+        ".company-info-container",
+      );
+      const header_placeholder = document.querySelector(".company-detail-container");
+
+      let detail_output = `
+        <div class="company-info">
+          <p id="job-detail-type">${job.postedAt} . ${job.contract}</p>
+          <h2 class="job-detail-title">${job.position}</h2>
+          <p id="job-detail-location">${job.location}</p>
+          <div class="company-site2-button">
+            <button type="submit" id="company-site2">Apply Now</button>
+          </div>
+          <p id="about-company">${job.description}</p>
+          <h2 id="company-requirements">Requirements</h2>
+          <p id="about-company">${job.requirements.content}</p>
+          <ul id="requirement-list">`;
+
+      job.requirements.items.forEach(function (item) {
+        detail_output += `<li>${item}</li>`;
+      });
+
+      detail_output += `
+          </ul>
+          <h2 id="company-requirements">What you will do</h2>
+          <p id="about-company">${job.role.content}</p>
+          <ol id="requirement-list">`;
+
+      job.role.items.forEach(function (item) {
+        detail_output += `<li>${item}</li>`;
+      });
+
+      detail_output += `
+          </ol>
+        </div>`;
+      let header_output = `
+        <div class="logo">
+        <img
+                class="detail-logo"
+                style="background-color:${job.logoBackground}"
+                src=".${job.logo}"
+                alt=""
+            />
+        </div>
+        <div class="company-name">
+        <h2 id="detail-company-name">${job.company}</h2>
+        <p id="company-website"><a href="${job.website}">${job.company}.com</a></p>
+        </div>
+
+        <div class="company-site-button">
+        <button type="submit" id="company-site">Company Site</button>
+        </div>      
+        `;
+
+      detail_placeholder.innerHTML = detail_output;
+      header_placeholder.innerHTML = header_output;
+    } else {
+      console.error("Job not found.");
+    }
+  });
